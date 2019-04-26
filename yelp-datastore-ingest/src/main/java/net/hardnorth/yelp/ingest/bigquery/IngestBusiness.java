3bd2aa3ef215ead6446a3aad245524408e210fc6
Vadzim Hushchanskou <del.grounder@gmail.com>
2019-04-26 23:22:44 +0300

    Adding State filering in business ingestion

--- yelp-datastore-ingest/src/main/java/net/hardnorth/yelp/ingest/bigquery/IngestBusiness.java
+++ yelp-datastore-ingest/src/main/java/net/hardnorth/yelp/ingest/bigquery/IngestBusiness.java
@@ -5,6 +5,7 @@ import com.google.api.services.bigquery.model.TableReference;
 import com.google.api.services.bigquery.model.TableRow;
 import com.google.api.services.bigquery.model.TableSchema;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.gson.JsonObject;
 import net.hardnorth.yelp.ingest.bigquery.conversions.JsonObjectToTableRow;
 import net.hardnorth.yelp.ingest.bigquery.conversions.StringToJsonObjectFunction;
@@ -19,7 +20,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Objects;
+import java.util.Set;
 
 import static org.apache.beam.sdk.values.TypeDescriptor.of;
 
@@ -46,6 +49,13 @@ public class IngestBusiness
                     .build()
     );
 
+    private static final Set<String> STATE_LIST = ImmutableSet.<String>builder()
+            .add("AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN")
+            .add("KS", "KY", "LA", "MA", "MD", "ME", "MH", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ")
+            .add("NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "PW", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI")
+            .add("VT", "WA", "WI", "WV", "WY")
+            .build();
+
     public static void main(String[] args)
     {
         LOGGER.info("Running with parameters:" + Arrays.asList(args).toString());
@@ -58,6 +68,9 @@ public class IngestBusiness
                         .into(of(JsonObject.class))
                         .via(new StringToJsonObjectFunction()))
                 .apply("Throw away null rows", Filter.by(Objects::nonNull))
+                .apply("Throw away non US businesses by state",
+                        Filter.by((e) -> STATE_LIST.contains(e.getAsJsonPrimitive("state").getAsString()
+                                .toUpperCase(Locale.US))))
                 .apply("Convert JsonObject to one step depth TableRow objects for BigQuery", MapElements
                         .into(of(TableRow.class))
                         .via(new JsonObjectToTableRow()))
